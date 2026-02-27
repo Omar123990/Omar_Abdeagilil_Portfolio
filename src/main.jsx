24b05ff67b3b5e8693f70a467a299ff8b6f65c75
Omar123990 <oalash55@gmail.com>
2026-02-27 08:34:36 +0200

    fix_pwa

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { registerSW } from 'virtual:pwa-register'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { registerSW } from "virtual:pwa-register";
 
-registerSW({ immediate: true })
-
-createRoot(document.getElementById('root')).render(
+if ("serviceWorker" in navigator) {
+  registerSW({
+    onNeedRefresh() {
+      if (confirm("New content available. Reload?")) {
+        window.location.reload();
+      }
+    },
+    onOfflineReady() {
+      console.log("App is ready to work offline");
+    },
+  });
+}
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
